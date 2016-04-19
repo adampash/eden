02b92745ae6a5f7dfbd6d52e33d2e3429b46e44b
Adam Pash <adam.pash@gmail.com>
2016-04-19 16:17:32 -0400

    Can add multiple comma-separated users at once

--- lib/hedwig/responders/twitter.ex
+++ lib/hedwig/responders/twitter.ex
@@ -5,21 +5,29 @@ defmodule Hedwig.Responders.Twitter do
   @usage """
   eden follow ev - follows a user and puts tweets in the current channel (or DMs them to you if you're not in a channel)
   """
-  respond ~r/follow\s+(\w+)/i, msg do
-    username = msg.matches[1]
-    resp = case ExTwitter.user_search(username, count: 1, include_entities: false) do
-      [] ->
-        "Hmm... couldn't find a twitter user called #{username} :thinking_face:"
-      [user] ->
+  respond ~r/follow\s+(.+)$/i, msg do
+    users = String.split(msg.matches[1], ~r/\s?,/)
+    IO.inspect users
+    for username <- users do
+      resp = try do
+        user = ExTwitter.user(username)
         Twitter.Users.follow({user.screen_name, user.id_str}, msg.room)
         link = "https://twitter.com/#{user.screen_name}/status/#{user.status.id}"
         """
-          You got it. I'll start following `#{username}` and posting new tweets to this channel.
-          Here's the latest from `#{username}`:
+          You got it. I'll start following `#{user.screen_name}` and posting new tweets to this channel.
+          Here's the latest from `#{user.screen_name}`:
           #{link}
         """
+      rescue ExTwitter.Error ->
+        case ExTwitter.user_search(username, count: 1, include_entities: false) do
+          [user] ->
+            "Hmm... couldn't find a twitter user called #{username} :thinking_face:. Maybe you meant #{user.screen_name}?"
+          _ ->
+            "Hmm... couldn't find a twitter user called #{username} :thinking_face:."
+        end
+      end
+      reply msg, resp
     end
-    reply msg, resp
   end
 
   @usage """
@@ -27,13 +35,16 @@ defmodule Hedwig.Responders.Twitter do
   """
   respond ~r/unfollow\s+(\w+)/i, msg do
     username = msg.matches[1]
-    resp = case ExTwitter.user_search(username, count: 1, include_entities: false) do
-      [] ->
-        "Hmm... I couldn't find a twitter user called #{username} :thinking_face:"
-      [user] ->
-        Twitter.Users.unfollow({user.screen_name, user.id_str}, msg.room)
-        "you got it. I'll stop following #{msg.matches[1]} for this channel"
-    end
+    user = ExTwitter.user(username)
+    Twitter.Users.unfollow({user.screen_name, user.id_str}, msg.room)
+    resp = "you got it. I'll stop following #{msg.matches[1]} for this channel"
+    # resp = case ExTwitter.user(username, count: 1, include_entities: false) do
+    #   user ->
+    #     Twitter.Users.unfollow({user.screen_name, user.id_str}, msg.room)
+    #     "you got it. I'll stop following #{msg.matches[1]} for this channel"
+      # _ ->
+      #   "Hmm... I couldn't find a twitter user called #{username} :thinking_face:"
+    # end
     reply msg, resp
   end
 
