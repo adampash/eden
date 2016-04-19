0f7ff04be79090ae8cc72bf850c797dcfbe70a44
Adam Pash <adam.pash@gmail.com>
2016-04-19 15:35:47 -0400

    Added archiving to persistence

--- lib/twitter/persistence.ex
+++ lib/twitter/persistence.ex
@@ -2,6 +2,7 @@ defmodule Twitter.Persistence do
   use GenServer
 
   def start_link(db_folder \\ "db") do
+    # :timer.send_interval(__MODULE__, :archive, 1000)
     GenServer.start_link(__MODULE__, db_folder, name: __MODULE__)
   end
 
@@ -13,8 +14,14 @@ defmodule Twitter.Persistence do
     GenServer.call(__MODULE__, {:lookup, key})
   end
 
+  def archive do
+    GenServer.call(__MODULE__, :archive)
+  end
+
   def init(db_folder) do
     File.mkdir_p(db_folder)
+    one_day = 86_400_000 # milliseconds
+    :timer.send_interval(one_day, __MODULE__, :archive)
     {:ok, db_folder}
   end
 
@@ -30,6 +37,28 @@ defmodule Twitter.Persistence do
       {:error, _reason} -> nil
     end
     {:reply, data, db_folder}
+  end
+
+  def handle_info(:archive, db_folder) do
+    IO.puts "Running daily archive"
+
+    File.mkdir_p("#{db_folder}/archive")
+    {:ok, files} = File.ls(db_folder)
+
+    files
+    |> Enum.filter(&(File.regular?("#{db_folder}/#{&1}")))
+    |> archive_files(db_folder)
+
+    {:noreply, db_folder}
+  end
+
+  defp archive_files(files, db_folder) do
+    for file <- files, do: archive(file, db_folder)
+  end
+
+  defp archive(file, db_folder) do
+    {{year, month, day}, _} = :calendar.universal_time
+    File.copy(file_name(db_folder, file), "#{db_folder}/archive/#{year}_#{month}_#{day}_#{file}")
   end
 
   defp file_name(db_folder, key), do: "#{db_folder}/#{key}"
